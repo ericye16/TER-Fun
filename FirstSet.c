6e8c77c271aa06abf4d33e4253a19884c3c44bbb
Eric Ye <computereric@live.ca>
2013-03-20 12:58:17 -0400

    Changed values, more hardware-y stuff

--- FirstSet.c
+++ FirstSet.c
@@ -1,12 +1,18 @@
 /* (c) Eric and Andrew
  */
 
-#define DEGREES_TO_TURN_RATIO 1 //change this later
+#define DEGREES_TO_TURN_RATIO 6.5
+#define CM_TO_TURN_RATIO 360.0/14.0
+//#define FRICTION_CM_TO_TURN_RATIO 360.0/8.5
+#define ROBOT_WIDTH 18.5
 
 void pointTurn(int degrees);
 void square(int distance);
 void acceleration(double seconds);
 void goCM(double n);
+void turnaround(double distance, char opt);
+void circle0(double radius, double degrees);
+void circle(double radius);
 
 task main()
 {
@@ -14,30 +20,21 @@ task main()
 	nSyncedTurnRatio = 100;
 	motor[motorB] = 100;
 	nMotorEncoder[motorB] = 0;
-	//nMotorEncoderTarget[motorB] = 360;
-	goCM(1000);
-	//motor[motorB] = 0;
-	//wait1Msec(1000);
-	//nMotorEncoder[motorA] = 0;
-	//nMotorEncoderTarget[motorA] = -180;
-	//motor[motorA] = -100;
-	//while(nMotorRunState[motorA] != runStateIdle);
-	//nMotorEncoderTarget[motorA] = 0;
-	//motor[motorA] = 100;
-	//while(nMotorRunState[motorA] != runStateIdle);
-
+	circle(0);
 
 }
 
 void acceleration(double seconds) {
-   nSyncedMotors = synchBC; //sync the movement motors
-   nSyncedTurnRatio = 100; //they move together
+   //nSyncedMotors = synchBC; //sync the movement motors
+   //nSyncedTurnRatio = 100; //they move together
    double step = seconds / 180.; //find the amount of time per percent change in motors
    for (int i = 10; i <= 100; i++) { //increase motor power from 10 to 100 _very_ gradually
+      motor[motorC] = i;
     	motor[motorB] = i;//move the motor that much power
     	wait1Msec((int) (step * 1000)); //wait a single "step" in time--total 180 steps will happen
    }
    for (int i = 100; i >= 10; i--) { //decrease the motor power from 100 to 10 _very_ gradually
+      motor[motorC] = i;
       motor[motorB] = i; //move that much
       wait1Msec((int) (step * 1000)); //wait that much
    }
@@ -64,6 +61,19 @@ void square(int distance)  {
   }
 }
 
+void circle(double radius) {
+   circle0(radius, 360);
+}
+
+void circle0(double radius, double degrees) {
+    nSyncedMotors = synchBC;
+    nSyncedTurnRatio = (int)100 * (radius / (radius + ROBOT_WIDTH));
+    int amountTurn = (int) ((degrees / 360.0) * (2 * PI * (radius + ROBOT_WIDTH)));
+    nMotorEncoder[motorB] = 0;
+    motor[motorB] = 100;
+    while(nMotorEncoder[motorB] < amountTurn * CM_TO_TURN_RATIO);
+}
+
 void pointTurn(int degrees) {
   nSyncedMotors = synchBC;
   nSyncedTurnRatio = -100; //make one wheel turn exactly opposite the other wheel's direction
@@ -77,5 +87,5 @@ void goCM(double n) {
     nSyncedTurnRatio = 100;
     nMotorEncoder[motorB] = 0; //reset the rotational sensor
     motor[motorB] = 100; //AHEAD
-    while(nMotorEncoder[motorB] < (int) n * 360.0/11.0); //keep going, magic number is 11
+    while(nMotorEncoder[motorB] < (int) n * CM_TO_TURN_RATIO); //keep going, magic number is 11
 }
